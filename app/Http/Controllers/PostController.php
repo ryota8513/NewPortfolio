bef6b8df4a24586ba6a2073867c0da97496863e0
畑本亮太 <hatamotoryouta@hatamotoryoutanoMacBook-Pro.local>
2021-03-14 13:34:50 +0900

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+   
+    public function index(){
+        return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="csrf-token" content="{{ csrf_token() }}"> 
+  <link rel="stylesheet" href="{{asset('/css/index.css')}}">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.9.0/css/flag-icon.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+  <title>旅BOOK</title>
+</head>
+<body>
+<header>
+ <div class="header">
+  <div class="header-top">
+   <div class="header-title">
+    <h1><img src="{{asset('/image/travel.png')}}" alt="titleアイコン" width="50px">旅BOOK</h1>
+    <p>〜みんなの思い出でつくるサイト</p>
+    </div>
+    <div class="site-date">
+      <h3>累計投稿数</h3>
+    </div>
+   <div class="user"> 
+     <div class="user-nav">
+       <li><a href="#">ようこそuserさん<span class="fas fa-angle-down"></span></a>
+        <ul>
+        <li><a href="#"><span class="fas fa-chevron-right"></span>マイページ</a></li>
+        <li><a href="#"><span class="fas fa-chevron-right"></span>編集</a></li>
+        <li><a href="#"><span class="fas fa-chevron-right"></span>ログアウト</a></li>
+        </ul>
+      </li>
+     </div>
+   </div>
+  <div class="site-var">
+   <ul>
+     <li><a href="#">ホーム</a></li>
+     <li><a href="#">サイトについて</a></li>
+     <li><a href="#">製作者について</a></li>
+   </ul>  
+  </div>
+ </div>
+</div>
+</header>
+<div class="slider-content">
+  <ul class="slider">
+     <li><img src="{{asset('/image/sample1.jpg')}}"></li>
+     <li><img src="{{asset('/image/sample2.jpg')}}"></li>
+     <li><img src="{{asset('/image/sample3.jpg')}}"></li>
+     <li><img src="{{asset('/image/sample4.jpg')}}"></li>
+     <li><img src="{{asset('/image/sample5.jpg')}}"></li>
+     <li><img src="{{asset('/image/sample6.jpg')}}"></li>
+  </ul>
+</div>
+<script src="{{asset('/assets/index.js')}}"></script>
+
+</body>
+</html>
+
+
+
+<div class="balloon2-top">
+</div>
