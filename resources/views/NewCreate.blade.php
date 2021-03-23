953106738967f38c58f4c8b5522664f0df48caa0
畑本亮太 <hatamotoryouta@hatamotoryoutanoMacBook-Pro.local>
2021-03-23 18:33:56 +0900

    編集

--- resources/views/NewCreate.blade.php
+++ resources/views/NewCreate.blade.php
@@ -10,15 +10,15 @@
   <div class="Form">
    <h1>新規会員登録</h1>
 
-   <div class="Form-Item">
+   <!-- <div class="Form-Item">
    <p class="Form-Item-Label">アイコン</p>
-   <input type="file" id="target" accept="image/*">
-   <!-- <img id="myImage"> -->
-   </div>
+   <input type="file" id="target" accept="image/*"  name="image">
+   <img id="myImage">
+   </div> -->
 
   <div class="Form-Item">
     <p class="Form-Item-Label">ユーザー名</p>
-    <input type="text" name="name" class="Form-Item-Input" placeholder="例）山田太郎" autofocus>
+    <input type="text" name="name" class="Form-Item-Input" placeholder="例）山田太郎" required autocomplete="name" autofocus>
     @error('name')
      <span class="invalid-feedback" role="alert">
          <strong>{{ $message }}</strong>
@@ -28,7 +28,7 @@
 
   <div class="Form-Item">
     <p class="Form-Item-Label">メールアドレス</p>
-    <input type="email" name="email" class="Form-Item-Input" placeholder="例）example@gmail.com" >
+    <input type="email" name="email" class="Form-Item-Input" placeholder="例）example@gmail.com"  required autocomplete="email">
     @error('email')
       <span class="invalid-feedback" role="alert">
           <strong>{{ $message }}</strong>
@@ -38,13 +38,19 @@
   
   <div class="Form-Item">
     <p class="Form-Item-Label">パスワード</p>
-    <input type="password" name="password" class="Form-Item-Input" placeholder="8文字以上で入力してください" autocomplete="off">
+    <input type="password" name="password" class="Form-Item-Input" placeholder="8文字以上で入力してください" required autocomplete="new-password">
     <!-- @error('password')
       <span class="invalid-feedback" role="alert">
          <strong>{{ $message }}</strong>
       </span>
     @enderror -->
   </div>
+
+  <div class="Form-Item">
+    <p class="Form-Item-Label">パスワード再入力</p>
+    <input type="password" name="password_confirmation" class="Form-Item-Input" placeholder="もう一度入力してください" required autocomplete="new-password" >
+  </div>
+  
   <button type="submit" class="Form-Btn" value="登録">登録</button>
 </form>
 <a class="Backlogin" href="./login">登録済みの方はこちらから</a>
